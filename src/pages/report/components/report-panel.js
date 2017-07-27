28861947869ce5d88cf699eb48f62f89959544c3
Tuan Phan <tuan230878@yahoo.com>
2017-07-27 16:27:47 +1000

    setting dimension for report container

--- src/pages/report/components/report-panel.js
+++ src/pages/report/components/report-panel.js
@@ -1,10 +1,17 @@
 import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {VictoryPie, VictoryChart, VictoryTheme, VictoryBar} from 'victory';
+import {VictoryPie, VictoryChart, VictoryTheme, VictoryBar, VictoryContainer} from 'victory';
 import { Grid, Row, Col } from 'react-bootstrap'
 class ReportPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        const containerWidth = (window.innerWidth / 3 < 300) ? window.innerWidth: (window.innerWidth / 3);
+        this.state = { containerWidth: containerWidth, containerHeight: 300 };
+        console.log(`report\components\report-panel window dimension is ${window.innerWidth}`);
+    }
     render() {
+        console.log(`report\components\report-panel start rendering reports`)
         const sampleData = [
             { x: 1, y: 2, label: "one" },
             { x: 2, y: 3, label: "two" },
@@ -19,35 +26,28 @@ class ReportPanel extends React.Component {
             { x: 11, y: 5, label: "three" },
             { x: 12, y: 5, label: "three" }
         ];
+        //const container = <VictoryContainer height={this.state.containerHeight} width={this.state.containerWidth}/>;
         return (
             <Grid>
-                <Row className='show-grid'>
-                    <Col sm={6}>
-                        <svg height='350px' width='570px'>
-                            <g>
-                                <VictoryPie theme={VictoryTheme.material} padAngle={3} innerRadius={100} data={sampleData}/>
-                            </g>
-                        </svg>
+                <Row className='show-grid' >
+                    <Col sm={6} style={{height:300}}>
+                       <VictoryPie theme={VictoryTheme.material} padAngle={3} innerRadius={100} data={sampleData}/>
                     </Col>
-                    <Col sm={6}>
-                        <svg height='350px' width='570px'>
-                            <g>
-                                <VictoryChart domainPadding={20}>
-                                    <VictoryBar data={sampleData}/>
-                                </VictoryChart>
-                            </g>
-                        </svg>
-                    </Col>
-                </Row>
-                <Row className='show-grid'>
-                    <Col sm={6}>
-                        <VictoryPie theme={VictoryTheme.material} data={sampleData}/>
-                    </Col>
-                    <Col sm={6}>
+                    <Col sm={6} style={{height:300}}>
                         <VictoryChart domainPadding={20}>
                             <VictoryBar data={sampleData}/>
                         </VictoryChart>
                     </Col>
+                </Row>
+                <Row className='show-grid'>
+                    <Col sm={6} style={{height:300}}>
+                        <VictoryPie theme={VictoryTheme.material} padAngle={3} innerRadius={100} data={sampleData}/>
+                    </Col>
+                    <Col sm={6} style={{height:300}}>
+                        <VictoryChart domainPadding={20}>
+                            <VictoryBar data={sampleData}/>
+                        </VictoryChart>
+                     </Col>
                 </Row>
             </Grid>
 
