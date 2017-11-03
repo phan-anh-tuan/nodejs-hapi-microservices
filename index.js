937f3f393521ba8c0e6162c32dc5323b5aaac741
Tuan Phan <tuan230878@yahoo.com>
2017-11-03 14:07:23 +1100

    disable email notification

--- index.js
+++ index.js
@@ -8,25 +8,29 @@ const schedule = require('node-schedule')
 
 Server((err,server) => {
                            if (err) { throw err };
-                           
+                            
                             const notification = new Notification(server);
+                            
                             schedule.scheduleJob('*/30 * * * * *', function(){
                                //console.log('30 second scheduled task is executing!');
+                               /*
                                 Async.series([  
-                                           //     notification.putOverdueRequestsInQueue,
                                                 notification.sendTaskCreationEmail
                                             ],
                                            (error, result) => { //console.log('30 second scheduled task finished execution!'); 
                                         });
+                                        */
                             });
                             schedule.scheduleJob('0 22 21 * * *', function(){
                                 console.log('Daily Scheduled task is executing!');
+                                /*
                                 Async.auto([  
                                                 notification.putOverdueRequestsInQueue,
                                                 notification.putUnattendedRequestsInQueue
-                                                //notification.sendTaskCreationEmail
                                             ],
                                            (error, result) => { console.log('Daily scheduled task finished execution!'); });
+                                           */
                             });         
+                            
                            console.log('server is running at ' + server.info.uri);
                        }); 
